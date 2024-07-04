0cbf5e860244beecfb238cfdbbf7b0f3f4939f6a
Seth Mwebi <sethmwebi27@gmail.com>
2024-07-04 18:13:01 +0300

    writes code for filter method array

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (arr, driver) => {
+  return arr.filter((item) => item.toLowerCase() === driver.toLowerCase());
+};
+
+const fuzzyMatch = (arr, l) => {
+  return arr.filter((item) => item.toLowerCase().startsWith(l.toLowerCase()));
+};
+
+const matchName = (arr, name) => {
+  return arr.filter((item) => item.name === name);
+};
